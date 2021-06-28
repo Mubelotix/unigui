7d94bcad1c523fad81d304a68a3e7e98369ab932
Mubelotix <mubelotix@gmail.com>
2021-06-28 12:33:25 +0200

    Fix CI

--- src/area.rs
+++ src/area.rs
@@ -39,7 +39,7 @@ impl<'a> std::fmt::Debug for Area<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Area")
             .field("rect", &self.rect)
-            .finish_non_exhaustive()
+            .finish() // TODO replace by finish_non_exhaustive once CI accepts it
     }
 }
 
