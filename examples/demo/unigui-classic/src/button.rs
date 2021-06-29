61a9298ad4dbd82599c0e551f9803d2712989e25
Mubelotix <mubelotix@gmail.com>
2021-06-29 12:24:36 +0200

    Start using lyon

--- examples/demo/unigui-classic/src/button.rs
+++ examples/demo/unigui-classic/src/button.rs
@@ -22,46 +22,45 @@ impl Widget for Button {
 
     fn render(&self, surface: Area) {
         use fgui::graphics::Vertex;
+        use lyon::tessellation::{geometry_builder::simple_builder, *};
+        use lyon::path::Path;
+        use lyon::math::{point, Point};
 
-        surface.backend.add_vertex(Vertex {
-            position: [surface.rect.top_left.0 + 2.0, surface.rect.top_left.1 + 2.0],
-            color: [1.0, 0.0, 0.0, 1.0],
-        });
-        surface.backend.add_vertex(Vertex {
-            position: [
-                surface.rect.bottom_right.0 - 2.0,
-                surface.rect.top_left.1 + 2.0,
-            ],
-            color: [1.0, 0.0, 0.0, 1.0],
-        });
-        surface.backend.add_vertex(Vertex {
-            position: [
-                surface.rect.top_left.0 + 2.0,
-                surface.rect.bottom_right.1 - 2.0,
-            ],
-            color: [1.0, 0.0, 0.0, 1.0],
-        });
+        // Create a simple path.
+        let mut path_builder = Path::builder();
+        path_builder.begin(point(surface.rect.top_left.0 + 0.0, surface.rect.top_left.1 + 0.0));
+        path_builder.line_to(point(surface.rect.top_left.0 + 10.0, surface.rect.top_left.1 + 20.0));
+        path_builder.line_to(point(surface.rect.top_left.0 + 20.0, surface.rect.top_left.1 + 0.0));
+        path_builder.line_to(point(surface.rect.top_left.0 + 10.0, surface.rect.top_left.1 + 10.0));
+        path_builder.end(true);
+        let path = path_builder.build();
 
-        surface.backend.add_vertex(Vertex {
-            position: [
-                surface.rect.top_left.0 + 2.0,
-                surface.rect.bottom_right.1 - 2.0,
-            ],
-            color: [1.0, 0.0, 0.0, 1.0],
-        });
-        surface.backend.add_vertex(Vertex {
-            position: [
-                surface.rect.bottom_right.0 - 2.0,
-                surface.rect.top_left.1 + 2.0,
-            ],
-            color: [1.0, 0.0, 0.0, 1.0],
-        });
-        surface.backend.add_vertex(Vertex {
-            position: [
-                surface.rect.bottom_right.0 - 2.0,
-                surface.rect.bottom_right.1 - 2.0,
-            ],
-            color: [1.0, 0.0, 0.0, 1.0],
-        });
+        // Create the destination vertex and index buffers.
+        let mut buffers: VertexBuffers<Point, u16> = VertexBuffers::new();
+
+        {
+            let mut vertex_builder = simple_builder(&mut buffers);
+
+            // Create the tessellator.
+            let mut tessellator = FillTessellator::new();
+
+            // Compute the tessellation.
+            let result = tessellator.tessellate_path(
+                &path,
+                &FillOptions::default(),
+                &mut vertex_builder
+            );
+            assert!(result.is_ok());
+        }
+
+        for i in buffers.indices {
+            let vertex = buffers.vertices[i as usize];
+            let vertex = Vertex {
+                position: [vertex.x, vertex.y],
+                color: [1.0, 0.0, 0.0, 1.0],
+            };
+            surface.backend.add_vertex(vertex);
+        }
+
     }
 }
